1fb43b3ca0db1d1387b453812bbb35abd0a2f939
Munna -Dev <mkcodes.in@gmail.com>
2026-03-18 13:37:05 +0530

    Blog hero section with search bar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,13 +2,18 @@ function Navbar() {
     return (
         <div className="flex items-center justify-between gap-2 bg-gray-100 py-4 px-8">
             <div className="flex items-center gap-2">
-                <h1 className="text-2xl font-bold text-gray-800">Blog App</h1>
+                <h1 className="text-2xl font-bold text-gray-800"><span className="text-blue-500 font-bold text-3xl">B</span>logSpace</h1>
             </div>
             <div className="flex gap-2">
-                <button className="bg-blue-500 text-white px-4 py-2 rounded-lg">Login</button>
+                <button className="relative px-6 py-1.5 font-semibold border-2 border-indigo-500 overflow-hidden group rounded cursor-pointer">
+                    <span className="absolute inset-0 bg-indigo-500 -translate-x-full group-hover:translate-x-0 transition-transform duration-300"></span>
+                    <span className="relative z-10 text-indigo-500 group-hover:text-white transition-colors duration-300">
+                        Login
+                    </span>
+                </button>
             </div>
-        </div>
+        </div >
     )
 }
 
-export default Navbar;  
+export default Navbar;
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,17 +1,19 @@
 import Navbar from "../components/Navbar";
 import BlogCard from "../components/BlogCard";
 import Footer from "../components/Footer";
+import Blog from "./Blog";
 
 function Home() {
   return (
     <div className="min-h-screen flex flex-col bg-slate-100">
       {/* Navbar */}
-        <div className="max-w-7xl mx-auto w-full px-4 pt-3">
-          <Navbar />
+      <div className="max-w-7xl mx-auto w-full px-4 pt-3">
+        <Navbar />
       </div>
 
       {/* Main Content */}
-      <div className="flex-1 max-w-7xl mx-auto w-full px-4">
+      <div className="flex-1 max-w-7xl mx-auto w-full px-4 space-y-4">
+        <Blog />
         <BlogCard />
       </div>
 
